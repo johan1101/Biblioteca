4e6a5fa48d3f56eab9151307514752a90e99b18c
gabrieladelgado26 <gabrieladelgado1081@gmail.com>
2023-10-29 21:38:51 -0500

    Agregando servlet para agregar libros

--- Biblioteca/src/main/java/Servlets/SvAgregarLibro.java
+++ Biblioteca/src/main/java/Servlets/SvAgregarLibro.java
@@ -1,16 +1,30 @@
 
 package Servlets;
 
+import Clases.Libros;
+import Clases.Lista;
+import Clases.Serializacion;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 @WebServlet(name = "SvAgregarLibro", urlPatterns = {"/SvAgregarLibro"})
 public class SvAgregarLibro extends HttpServlet {
+    
+    // Crear una instancia de la clase ListasEnlazadas
+    Lista listaEnlazada = new Lista();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -64,7 +78,53 @@ public class SvAgregarLibro extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        // Obtener el contexto del servlet
+        ServletContext context = getServletContext();
+
+        try {
+            listaEnlazada = Serializacion.leerArchivoLibros(context);
+            if (listaEnlazada == null) {
+                listaEnlazada = new Lista();
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SvAgregarLibro.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // Obtener los datos del formulario enviados por POST
+        String titulo = request.getParameter("titulo");
+        String autor = request.getParameter("autor");
+        String fecha = request.getParameter("anioPublicacion");
+        //Obtener la parte del archivo de imagen desde la solicitud
+        Part filePart = request.getPart("fotoPortada");
+        
+        // Directorio de carga en el servidor donde se guardarán las imágenes
+        String uploadPath = context.getRealPath("/imgPerros");
+
+        // Obtener el nombre del archivo de imagen enviado
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+
+        // Construir la ruta completa del archivo de imagen en el servidor
+        String filePath = uploadPath + File.separator + fileName;
+
+        //Abrir un flujo de entrada para el archivo de imagen recibido
+        try (InputStream fileContent = filePart.getInputStream(); FileOutputStream outputStream = new FileOutputStream(filePath)) {
+
+            int read;
+            byte[] buffer = new byte[1024];
+
+            //Leer el archivo de imagen y escribirlo en el servidor
+            while ((read = fileContent.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+        }
+        
+        Libros libro = new Libros(titulo, autor, fecha, fileName);
+        
+        listaEnlazada.agregarAlFinal(libro);
+        Serializacion.escribirArchivoLibros(listaEnlazada, context);
+        
+        response.sendRedirect("biblioteca.jsp");
     }
 
     /**
